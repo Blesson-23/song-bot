88b3a2faed719e3daf2ab96519d0b8bd9030fd25
Blesson-23 <94548940+Blesson-23@users.noreply.github.com>
2021-11-22 18:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://cdn.vox-cdn.com/thumbor/WGdCHzenDoxjeFD9YoNtKUwvfLA=/0x0:3000x2000/1220x813/filters:focal(1260x760:1740x1240):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/66801071/jomi_avatar_nickleodeon_ringer.0.jpg")
     OWNER = os.environ.get("OWNER", "shamilhabeeb") 
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
